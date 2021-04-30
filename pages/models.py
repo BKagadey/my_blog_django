81fd94272ebc1e27a0fece7ac286a5a4e301c329
Bohdan Kahadei <kagadey2@gmail.com>
2021-04-30 22:34:46 +0300

    Add PostgreSQL

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Pages(models.Model):
+    page_image = models.ImageField(upload_to='page_images/')
+    page_text = models.CharField(max_length=300)
